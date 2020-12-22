ec6c042305f7e1e95d92f22b4b1da82baad1f938
ka2nq1 <ka2nq1@gmail.com>
2020-12-22 14:58:16 +0200

    complete

--- src/components/developers/js/index.js
+++ src/components/developers/js/index.js
@@ -7,10 +7,12 @@ const studentBtn = document.querySelector('.js-students');
 studentBtn.addEventListener('click', hendelOpenStudentModal);
 function hendelOpenStudentModal(e) {
   modalBackDrop();
+  const body = document.querySelector('body');
   const container = document.querySelector('.modal');
   const closeBnt = document.querySelector('.student-modal__close-btn');
   function closeModal() {
     container.classList.remove('show-modal');
+    body.style.overflow = 'unset';
   }
   closeBnt.addEventListener('click', closeModal);
 }
